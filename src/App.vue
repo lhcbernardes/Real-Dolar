40a2d33a88fa2276dbdd47093f05218d9c785a9b
lhcbernardes <lhcb@a.recife.ifpe.edu.br>
2021-04-24 22:32:50 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Toolbar>
+    <template #left>
+    <h3>Reais para Dólares</h3>
+    </template>
+
+    <template #right>
+    </template>
+</Toolbar>
+ <h1 class="label-title">$</h1>
+ <div class="container">
+  <div class="div-imc">
+    <span class="p-float-label">
+     <InputText id="input-weight" type="text" v-model="reais"/>
+        <label for="inputtext">Quantidade em Reais</label>
+    </span>
+  </div>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-height" type="text" v-model="dolar"/>
+      <label for="input-height">Cotação do Dólar</label>
+    </span>
+  </div>
+ </div>
+  <div class="div-imc">
+    <Button label="Limpar" @click="clear()"/>
+    <Button class="button-calculate" label="Calcular" @click="calculate()"/>
+  </div>
+
+  <div v-if="resultado" class="container">
+    <p class="label-result">Com {{reais}} reais é possível comprar {{resultado}} dólares a {{dolar}} cada.</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      reais: null,
+      dolar: null,
+      resultado: null
+    }
+  },
+  methods: {
+    calculate: function() {
+      this.resultado = (this.reais/this.dolar).toFixed(2);
+    },
+    clear() {
+      this.reais = null;
+      this.dolar = null;
+      this.resultado = null;
+    }
   }
 }
 </script>
@@ -21,6 +62,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.div-imc {
+margin-top: 2rem;
+}
+
+.label-title {
+  font-size: 3rem;
+}
+
+.label-subtitle {
+  font-size: 1.5rem;
+}
+
+.label-result{
+font-size: 2rem;
+}
+
+.button-calculate{
+margin-left: 1rem !important;
+}
+
+.container{
+  margin-left: auto;
+    margin-right: auto;
+    width: 20rem;
+}
+
+.p-inputtext{
+  width: 20rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import Toolbar from 'primevue/toolbar';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(PrimeVue);
+app.component("Button", Button);
+app.component("InputText", InputText);
+app.component("Toolbar", Toolbar);
+
+app.mount('#app');
